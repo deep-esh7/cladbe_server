d6bf40cd2a5f76f71cdcf075bade9a9612ae9199
Deepesh Kumar <dkdeepeshkumar5@gmail.com>
2025-01-21 14:54:20 +0530

    sql query builder

---
+++ src/test/helpers/chathelper.js
@@ -0,0 +1,35 @@
+const { generateUniqueId } = require("../helpers/randomId");
+const { ChatRoomModel, ChatModel, ChatContentType } = require("../models/lead_model");
+
+function createChatRoomModel(participantsList) {
+    if (!Array.isArray(participantsList)) {
+        throw new Error('participantsList must be an array');
+    }
+    return new ChatRoomModel(
+        generateUniqueId(),
+        new Date().toISOString(),
+        participantsList
+    );
+}
+
+function createChatMessage(companyName, companyId, text) {
+    const messageId = generateUniqueId();
+
+    const currentDate = new Date();
+
+    // Formatting the date to 'YYYY-MM-DD HH:mm:ss.SSS'
+    const formattedDateTime = currentDate.toISOString().replace('T', ' ').slice(0, -1);
+
+    return new ChatModel({
+        messageId: messageId,
+        senderId: companyId,
+        dateTime: formattedDateTime,
+        contentType: ChatContentType.SYSTEM_GENERATED.value,
+        contentUrl: '',
+        text: text,
+        name: companyName,
+        phoneNumber: ""
+    });
+}
+
+module.exports = { createChatRoomModel, createChatMessage };
---
+++ src/test/helpers/media_controller.js
@@ -0,0 +1,39 @@
+const MediaService = require("./media_service");
+class MediaController {
+  static async uploadMedia(req, res) {
+    const files = req.files;
+    const directoryPath = req.body.directory; // Extract directory path from the request body
+    console.log(req.files);
+    console.log(req.body.directory);
+    if (!files) {
+      return res.status(400).send({ error: "No files uploaded." });
+    }
+
+    try {
+      const mediaArray = await MediaService.uploadMedia(files, directoryPath);
+
+      res
+        .status(200)
+        .send({ message: "Files uploaded successfully.", media: mediaArray });
+    } catch (err) {
+      res.status(500).send({ error: err.message });
+    }
+  }
+  static async delete(req, res) {
+    var filePath = req.body.filePath;
+
+    try {
+      // Initialize the Firebase Admin SDK
+      await MediaService.delete(filePath);
+
+      res.status(200).send({ message: "File deleted successfully." });
+    } catch (error) {
+      console.error("Error deleting file:", error);
+      res.status(500).send({
+        error: `An error occurred while deleting the file.  ${error}`,
+      });
+    }
+  }
+}
+
+module.exports={MediaController};
