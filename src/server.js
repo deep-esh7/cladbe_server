2dd5190b376ce111826f3986ea908ae659ce38c7
Deepesh Kumar <dkdeepeshkumar5@gmail.com>
2025-01-23 17:42:31 +0530

    fix

--- src/server.js
+++ src/server.js
@@ -605,7 +605,6 @@ async function gracefulShutdown(signal) {
       websocket: false,
       database: false,
     };
-    //empty
 
     // Close WebSocket connections gracefully
     if (wsHandler) {
